02bea66c820ce37e73c4e2b176827e42089327fb
Shadi Massalha <Shadi Massalha@ms-lap>
2013-11-06 02:20:44 +0200

    rename system test

--- src/stest/java/com/gigaspaces/persistency/MongoSystemTestSuite.java
+++ src/stest/java/com/gigaspaces/persistency/MongoSystemTestSuite.java
@@ -11,27 +11,43 @@ import com.gigaspaces.persistency.helper.MongoDBController;
 
 @RunWith(Suite.class)
 @SuiteClasses(value = {
-		/*BasicMongoSpaceAllInCacheTest.class,
-		BasicMongoSpaceLRUTest.class,*/
-		BasicMongoSpacePartialUpdateTest.class
-})
+/*
+ * BasicMongoSpaceAllInCacheTest.class, BasicMongoSpaceLRUTest.class,
+ * 
+ * BasicMongoSpacePartialUpdateTest.class
+ */
+SpaceMongoInitalLoadTest.class })
 public class MongoSystemTestSuite {
-	
+
 	private static final GSAgentController GS_AGENT_CONTROLLER = new GSAgentController();
 	private static final MongoDBController MONGO_DB_CONTROLLER = new MongoDBController();
 
 	@BeforeClass
 	public static void beforeSuite() {
-		
+
 		MONGO_DB_CONTROLLER.start();
-		
+
+		startGSAgent();
+	}
+
+	/**
+	 * start gs agent
+	 */
+	public static void startGSAgent() {
 		GS_AGENT_CONTROLLER.start();
 	}
 
 	@AfterClass
 	public static void afterSuite() {
-		GS_AGENT_CONTROLLER.stop();
-		
+		stopGSAgent();
+
 		MONGO_DB_CONTROLLER.stop();
+	}
+
+	/**
+	 * stop GS Agent
+	 */
+	public static void stopGSAgent() {
+		GS_AGENT_CONTROLLER.stop();
 	}
 }
